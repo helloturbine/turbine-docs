c8f61a6f2bf36d8991096641876e554ccd01ee43
Emilie Lima Schario <14057155+emilieschario@users.noreply.github.com>
2023-08-18 10:07:29 -0400

    Update records index (#87)
    * update records index
    
    * add empty file
    
    * Update docs/records/_category_.json
    
    * remove file
    
    * fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.helloturbine.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'helloturbine', // Usually your GitHub org/user name.
+  projectName: 'turbine-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -96,7 +96,7 @@ const config = {
               },
               {
                 label: 'Records',
-                to: '/records',
+                to: '/category/records',
               },
             ],
           },
